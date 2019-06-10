1be4a155550eb118e6fe58a02d9b2d0dd96a6963
Maryna KOTYTSKAYA <mkotytsk@e1r1p14.unit.ua>
2019-06-10 14:39:51 +0300

    done name split

--- asm/src/save_name.c
+++ asm/src/save_name.c
@@ -53,43 +53,60 @@ void	check_name_comment(int fd, char *line, t_all *champ)
 	char **str;
 
 	str = ft_strsplit_new(line);
-	if (!str || (!ft_strequ(str[0], ".name") && !ft_strequ(str[0], ".comment")))
+	if (!str || (!ft_strstr(line, ".name") && !ft_strstr(line, ".comment")))//(!ft_strequ(str[0], ".name") && !ft_strequ(str[0], ".comment")))
 		p_error("\nERROR! Invalid command.\n");
-	if (ft_strequ(str[0], ".name"))
+	if (ft_strstr(line, ".name"))//ft_strequ(str[0], ".name"))
 		init_name(fd, champ, line);
-	else
+	else if (ft_strstr(line, ".comment"))
 		init_comment(fd, champ, line);
 	del_arr(str);
 }
 
-char *find_string_tail(char *line, int index, int fd)//, char *str)
+char *save_end_of_command(char *buf, char *line, char *str)
 {
 	int		i;
-	char	*buf;
-	char	*str;
 
-	str = ft_strdup(line + index + 1);
+	i = 0;
+	while (line[i] != '"')
+		i++;
+	check_tail(line + i + 1);
+	buf = ft_strnew(i + 1);
+	ft_strncpy(buf, line, i);
 	free(line);
-	buf = NULL;
-	while (get_next_line(fd, &line))// && !ft_strchr(line, '"'))
-	{
-		if (ft_strchr(line, '"') && (i = -1))
-		{
-			while (line[++i] != '"')
-				if (line[i] == COMMENT_CHAR || line[i] == ALT_COMMENT_CHAR)
-					return (NULL);
-			buf = ft_strnew(i + 1);
-			ft_strncpy(buf, line, i);
-			break ;
-		}
-		else if (line[0] != '#')
-			p_error("\nERROR! Invalid data in command line.\n");
-		free(line);
-	}
 	line = ft_strjoin(str, buf);
+	// printf("[%s] [%s] {%c} [%s]\n", line, buf, buf[0], str);
 	free(str);
 	free(buf);
-	// printf("line = [%s] str = [%s] join = [%s]\n", line, str, ft_strjoin(str, buf));			
+	return (line);
+}
+
+char *find_string_tail(char *str, char *line, int fd)
+{
+	char	*buf;
+
+	buf = NULL;
+	while (get_next_line(fd, &line))
+	{
+		if (ft_strchr(line, '"'))
+		{
+			// char *d = ft_strnew(20);
+			// d = "12345";
+			// printf("-----%zu\n", ft_strlen(d));
+
+			// printf("LLL %s\n", line);
+			line = save_end_of_command(buf, line, str);
+			break ;
+		}
+		else
+		{
+			buf = ft_strjoin(str, line);
+			free(str);
+			str = ft_strdup(buf);
+			free(buf);
+		}
+		free(line);
+	}
+	// printf("LLL %s\n", line);
 	return (line);
 }
 
@@ -105,13 +122,12 @@ char 	*save_command_data(int fd, char *line, int index)
 	i = index;
 	while (line[++i] != '"')
 	if (line[i] == '\0')
-		if ((str2 = find_string_tail(line, index, fd)) != NULL)
+		if ((str2 = find_string_tail(ft_strdup(line + index + 1), line, fd)))
 			return (str2);
-	if (check_tail(line + i + 1))
-		p_error("\nERROR! Invalid data in command line.\n");
+	check_tail(line + i + 1);
 	str = ft_strnew(i - index + 1);
 	ft_strncpy(str, (line + (index + 1)), ((i - 1) - index));
-	printf("str =  %s\n", str);
+	printf("str =  %s line = [%s]\n", str, line);
 	return (str);
 }
 
